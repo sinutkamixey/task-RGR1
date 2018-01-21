01cd94ec390dc8477b1f3f13d2a77fe0e2f2c51c
sinutkamixey <sinutkamixey>
2018-01-21 21:48:32 +0600

    var 4

---
+++ var4/index.php
@@ -0,0 +1,486 @@
+﻿<?php
+ $data = array (
+  0 => 
+  array (
+    'id' => 12180059,
+    'name' => 'BC6712',
+    'description' => 'культиватор,  4х тактный двигатель 196 куб. см, , бензиновый',
+    'categoryId' => 267389,
+    'category' => 
+    array (
+      'id' => 267389,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мотоблоки и культиваторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '20800',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/12180059.jpeg',
+      'width' => 516,
+      'height' => 562,
+    ),
+    'link' => 'https://market.yandex.ru/product/12180059?hid=267389&pp=1001',
+    'vendorId' => 1030235,
+    'vendor' => 'Champion',
+    'isGroup' => false,
+    'reviewsCount' => 3,
+    'rating' => -1.0,
+    'offersCount' => 36,
+    'gradeCount' => 0,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  1 => 
+  array (
+    'id' => 12180166,
+    'name' => 'МБ-2Б-6.5 Pro',
+    'description' => 'мотоблок,  4х тактный двигатель 205 куб. см, вал отбора мощности, бензиновый',
+    'categoryId' => 267389,
+    'category' => 
+    array (
+      'id' => 267389,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мотоблоки и культиваторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '41900',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/12180166.jpeg',
+      'width' => 404,
+      'height' => 364,
+    ),
+    'link' => 'https://market.yandex.ru/product/12180166?hid=267389&pp=1001',
+    'vendorId' => 10717957,
+    'vendor' => 'Нева',
+    'isGroup' => false,
+    'reviewsCount' => 3,
+    'rating' => -1.0,
+    'offersCount' => 17,
+    'gradeCount' => 0,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  2 => 
+  array (
+    'id' => 13009114,
+    'name' => 'Урал',
+    'description' => 'мотоблок,  4х тактный двигатель 220 куб. см, вал отбора мощности, бензиновый',
+    'categoryId' => 267389,
+    'category' => 
+    array (
+      'id' => 267389,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мотоблоки и культиваторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '29999',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13009114.jpeg',
+      'width' => 701,
+      'height' => 649,
+    ),
+    'link' => 'https://market.yandex.ru/product/13009114?hid=267389&pp=1001',
+    'vendorId' => 6490943,
+    'vendor' => 'PATRIOT',
+    'isGroup' => false,
+    'reviewsCount' => 0,
+    'rating' => -1.0,
+    'offersCount' => 10,
+    'gradeCount' => 0,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  3 => 
+  array (
+    'id' => 12262970,
+    'name' => '09Н-01',
+    'description' => 'мотоблок,  4х тактный двигатель 270 куб. см, вал отбора мощности, бензиновый',
+    'categoryId' => 267389,
+    'category' => 
+    array (
+      'id' => 267389,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мотоблоки и культиваторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '77990',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/12262970.jpeg',
+      'width' => 701,
+      'height' => 506,
+    ),
+    'link' => 'https://market.yandex.ru/product/12262970?hid=267389&pp=1001',
+    'vendorId' => 11913160,
+    'vendor' => 'Беларус',
+    'isGroup' => false,
+    'reviewsCount' => 0,
+    'rating' => -1.0,
+    'offersCount' => 11,
+    'gradeCount' => 0,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  4 => 
+  array (
+    'id' => 13973646,
+    'name' => 'BC9713',
+    'description' => 'мотоблок,  4х тактный двигатель 212 куб. см, вал отбора мощности, бензиновый',
+    'categoryId' => 267389,
+    'category' => 
+    array (
+      'id' => 267389,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мотоблоки и культиваторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '27700',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13973646.jpeg',
+      'width' => 652,
+      'height' => 585,
+    ),
+    'link' => 'https://market.yandex.ru/product/13973646?hid=267389&pp=1001',
+    'vendorId' => 1030235,
+    'vendor' => 'Champion',
+    'isGroup' => false,
+    'reviewsCount' => 0,
+    'rating' => -1.0,
+    'offersCount' => 17,
+    'gradeCount' => 0,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  5 => 
+  array (
+    'id' => 12181000,
+    'name' => 'VARIO 60S TWK+',
+    'description' => 'мотоблок,  4х тактный двигатель 169 куб. см, вал отбора мощности, бензиновый',
+    'categoryId' => 267389,
+    'category' => 
+    array (
+      'id' => 267389,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мотоблоки и культиваторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '57990',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/12181000.jpeg',
+      'width' => 574,
+      'height' => 536,
+    ),
+    'link' => 'https://market.yandex.ru/product/12181000?hid=267389&pp=1001',
+    'vendorId' => 1020951,
+    'vendor' => 'CAIMAN',
+    'isGroup' => false,
+    'reviewsCount' => 2,
+    'rating' => -1.0,
+    'offersCount' => 50,
+    'gradeCount' => 0,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  6 => 
+  array (
+    'id' => 12180081,
+    'name' => 'МБ-2С-7.5 Pro',
+    'description' => 'мотоблок,  4х тактный двигатель 211 куб. см, , бензиновый',
+    'categoryId' => 267389,
+    'category' => 
+    array (
+      'id' => 267389,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мотоблоки и культиваторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '49500',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/12180081.jpeg',
+      'width' => 364,
+      'height' => 420,
+    ),
+    'link' => 'https://market.yandex.ru/product/12180081?hid=267389&pp=1001',
+    'vendorId' => 10717957,
+    'vendor' => 'Нева',
+    'isGroup' => false,
+    'reviewsCount' => 1,
+    'rating' => -1.0,
+    'offersCount' => 15,
+    'gradeCount' => 0,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  7 => 
+  array (
+    'id' => 12180465,
+    'name' => 'T 2000E',
+    'description' => 'культиватор,  двигатель, , электрический',
+    'categoryId' => 267389,
+    'category' => 
+    array (
+      'id' => 267389,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мотоблоки и культиваторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '19990',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/12180465.jpeg',
+      'width' => 598,
+      'height' => 701,
+    ),
+    'link' => 'https://market.yandex.ru/product/12180465?hid=267389&pp=1001',
+    'vendorId' => 214709,
+    'vendor' => 'Hyundai',
+    'isGroup' => false,
+    'reviewsCount' => 2,
+    'rating' => -1.0,
+    'offersCount' => 27,
+    'gradeCount' => 0,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  8 => 
+  array (
+    'id' => 12180080,
+    'name' => 'МБ-2С-6.5 Pro',
+    'description' => 'мотоблок,  4х тактный двигатель 169 куб. см, , бензиновый',
+    'categoryId' => 267389,
+    'category' => 
+    array (
+      'id' => 267389,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мотоблоки и культиваторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '47000',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/12180080.jpeg',
+      'width' => 364,
+      'height' => 420,
+    ),
+    'link' => 'https://market.yandex.ru/product/12180080?hid=267389&pp=1001',
+    'vendorId' => 10717957,
+    'vendor' => 'Нева',
+    'isGroup' => false,
+    'reviewsCount' => 0,
+    'rating' => -1.0,
+    'offersCount' => 19,
+    'gradeCount' => 0,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  9 => 
+  array (
+    'id' => 12180083,
+    'name' => 'МБ-23Б-10.0',
+    'description' => 'мотоблок,  двигатель, , бензиновый',
+    'categoryId' => 267389,
+    'category' => 
+    array (
+      'id' => 267389,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мотоблоки и культиваторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '54890',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/12180083.jpeg',
+      'width' => 348,
+      'height' => 396,
+    ),
+    'link' => 'https://market.yandex.ru/product/12180083?hid=267389&pp=1001',
+    'vendorId' => 10717957,
+    'vendor' => 'Нева',
+    'isGroup' => false,
+    'reviewsCount' => 1,
+    'rating' => -1.0,
+    'offersCount' => 18,
+    'gradeCount' => 0,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+);
+?>
+<p align="center" ><b>
+<?php
+echo "ПРАЙС-ЛИСТ НА МОТОКУЛЬТИВАТОРЫ";
+?>
+</b>
+</p>
+<table border align="center" cellspacing="0" cellpadding="5" cols="6" >
+	<tr>
+		<td>
+		<?php
+		echo "Название";
+		?>
+		</td>
+		<td>
+		<?php
+		echo "Описание";
+		?>
+		</td>
+		<td>
+		<?php
+		echo "Продавец";
+		?>
+		</td>
+		<td>
+		<?php
+		echo "Рейтинг";
+		?>
+		</td>
+		<td>
+		<?php
+		echo "Цена";
+		?>
+		</td>
+		<td>
+		<?php
+		echo "Фотография";
+		?>
+		</td>
+	</tr>
+	<?php
+	$newprices=[];
+	foreach($data as $index => $item){
+		$newprices[$index]=$item['prices']['amount'];
+	}
+	asort($newprices);
+
+	foreach($newprices as $index => $price){
+		foreach($data as $indexx => $item){
+			if($index==$indexx){
+			?>
+			<tr>
+				<td>
+				<?php
+				echo htmlspecialchars ($item['name']);
+				?>
+				</td>
+				<td>
+				<?php
+				echo htmlspecialchars($item['description']);
+				?>
+				</td>
+				<td>
+				<?php
+				echo htmlspecialchars ($item['vendor']);
+				?>
+				</td>
+				<td>
+				<?php
+				echo htmlspecialchars ($item['rating']);
+				?>
+				</td>
+				<td>
+				<?php
+				$formatedprice = number_format($price, 0, ',', ' ');
+				echo htmlspecialchars("$formatedprice ");
+				echo htmlspecialchars ($item['prices']['curName']);
+				?>
+				</td>
+				<td>
+				<?php
+				$photo=$item['mainPhoto']['url'];
+				$pwidth=$item['mainPhoto']['width']/6;
+				$pheight=$item['mainPhoto']['height']/6;
+				echo '<center><img src="'.htmlspecialchars($photo).'" width='.htmlspecialchars($pwidth).' height='.htmlspecialchars($pheight).'></center>';
+				?>
+				</td>
+			</tr>
+			<?php
+			}
+		}
+	}
+	?>
+</table>
+
+<?php
+$line=0;
+$vendors=[];
+foreach ($data as $index => $tablet){
+	$line=$tablet['vendor'];
+	if(in_array($line,$vendors)){
+	}
+	else {
+		$vendors[]=$line;
+	}
+}
+
+echo "<p>" ;
+echo '<div class="data">';
+echo "<h2><b> Список производителей: </b></h2>";
+echo '</div>' ;
+foreach($vendors as $vendor){
+	$k=0;
+	foreach ($data as $tablet){
+		if($vendor==$tablet['vendor']){
+			$k=$k+1;
+		}
+	}
+	echo '<div class="data">';
+	echo "<p>". htmlspecialchars($vendor)."</p>";
+	echo "<p>"."Количество предложений: $k"."</p>";
+	echo '</div>' ;
+}
