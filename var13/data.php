e85881e277f559eb14971c010da72f3dc2b58c9b
sinutkamixey <sinutkamixey>
2018-01-21 21:55:34 +0600

    var 13

---
+++ var13/data.php
@@ -0,0 +1,427 @@
+﻿<link rel="stylesheet" href="style.css">
+<?php
+ $data = array (
+  0 => 
+  array (
+    'id' => 7800733,
+    'name' => 'eTrex 10',
+    'kind' => 'навигатор',
+    'description' => 'универсальный, ПО: Garmin, 2.20", GPS+ГЛОНАСС, USB',
+    'categoryId' => 294661,
+    'category' => 
+    array (
+      'id' => 294661,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'GPS-навигаторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '8625',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/7800733.jpeg',
+      'width' => 408,
+      'height' => 701,
+    ),
+    'link' => 'https://market.yandex.ru/product/7800733?hid=294661&pp=1001',
+    'vendorId' => 433112,
+    'vendor' => 'Garmin',
+    'isGroup' => false,
+    'reviewsCount' => 29,
+    'rating' => 4.0,
+    'offersCount' => 36,
+    'gradeCount' => 59,
+    'articlesCount' => 1,
+    'isNew' => 0,
+  ),
+  1 => 
+  array (
+    'id' => 12659422,
+    'name' => 'eTrex 20x',
+    'kind' => 'навигатор',
+    'description' => 'универсальный, ПО: Garmin, 2.20", GPS+ГЛОНАСС, фото, USB, 3788Мб памяти',
+    'categoryId' => 294661,
+    'category' => 
+    array (
+      'id' => 294661,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'GPS-навигаторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '16140',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/12659422.jpeg',
+      'width' => 317,
+      'height' => 558,
+    ),
+    'link' => 'https://market.yandex.ru/product/12659422?hid=294661&pp=1001',
+    'vendorId' => 433112,
+    'vendor' => 'Garmin',
+    'isGroup' => false,
+    'reviewsCount' => 2,
+    'rating' => -1.0,
+    'offersCount' => 37,
+    'gradeCount' => 0,
+    'articlesCount' => 1,
+    'isNew' => 0,
+  ),
+  2 => 
+  array (
+    'id' => 10719288,
+    'name' => 'GPSMAP 64ST',
+    'kind' => 'навигатор',
+    'description' => 'универсальный, ПО: Garmin, 2.60", GPS+ГЛОНАСС, Bluetooth, компас, фото, USB, 8192Мб памяти',
+    'categoryId' => 294661,
+    'category' => 
+    array (
+      'id' => 294661,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'GPS-навигаторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '26480',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/10719288.jpeg',
+      'width' => 221,
+      'height' => 558,
+    ),
+    'link' => 'https://market.yandex.ru/product/10719288?hid=294661&pp=1001',
+    'vendorId' => 433112,
+    'vendor' => 'Garmin',
+    'isGroup' => false,
+    'reviewsCount' => 6,
+    'rating' => 4.5,
+    'offersCount' => 42,
+    'gradeCount' => 14,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  3 => 
+  array (
+    'id' => 10719287,
+    'name' => 'GPSMAP 64',
+    'kind' => 'навигатор',
+    'description' => 'универсальный, ПО: Garmin, 2.60", фото, USB, 4096Мб памяти',
+    'categoryId' => 294661,
+    'category' => 
+    array (
+      'id' => 294661,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'GPS-навигаторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '25790',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/10719287.jpeg',
+      'width' => 228,
+      'height' => 557,
+    ),
+    'link' => 'https://market.yandex.ru/product/10719287?hid=294661&pp=1001',
+    'vendorId' => 433112,
+    'vendor' => 'Garmin',
+    'isGroup' => false,
+    'reviewsCount' => 4,
+    'rating' => 4.5,
+    'offersCount' => 39,
+    'gradeCount' => 9,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  4 => 
+  array (
+    'id' => 12659483,
+    'name' => 'eTrex 30x',
+    'kind' => 'навигатор',
+    'description' => 'универсальный, ПО: Garmin, 2.20", GPS+ГЛОНАСС, компас, фото, USB, 3788Мб памяти',
+    'categoryId' => 294661,
+    'category' => 
+    array (
+      'id' => 294661,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'GPS-навигаторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '20290',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/12659483.jpeg',
+      'width' => 398,
+      'height' => 701,
+    ),
+    'link' => 'https://market.yandex.ru/product/12659483?hid=294661&pp=1001',
+    'vendorId' => 433112,
+    'vendor' => 'Garmin',
+    'isGroup' => false,
+    'reviewsCount' => 5,
+    'rating' => 3.5,
+    'offersCount' => 36,
+    'gradeCount' => 5,
+    'articlesCount' => 2,
+    'isNew' => 0,
+  ),
+  5 => 
+  array (
+    'id' => 12651438,
+    'name' => 'Astro 320 с ошейником T5',
+    'kind' => 'навигатор',
+    'description' => 'универсальный, ПО: Garmin, 2.60", барометр, компас, 1700Мб памяти',
+    'categoryId' => 294661,
+    'category' => 
+    array (
+      'id' => 294661,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'GPS-навигаторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '59190',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/12651438.jpeg',
+      'width' => 664,
+      'height' => 701,
+    ),
+    'link' => 'https://market.yandex.ru/product/12651438?hid=294661&pp=1001',
+    'vendorId' => 433112,
+    'vendor' => 'Garmin',
+    'isGroup' => false,
+    'reviewsCount' => 0,
+    'rating' => -1.0,
+    'offersCount' => 28,
+    'gradeCount' => 0,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  6 => 
+  array (
+    'id' => 11551476,
+    'name' => 'SA5 HD+',
+    'kind' => 'навигатор',
+    'description' => 'автомобильный, ПО: Навител, 5", Bluetooth, MP3, фото, видео, USB, 4096Мб памяти',
+    'categoryId' => 294661,
+    'category' => 
+    array (
+      'id' => 294661,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'GPS-навигаторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '4839',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/11551476.jpeg',
+      'width' => 599,
+      'height' => 442,
+    ),
+    'link' => 'https://market.yandex.ru/product/11551476?hid=294661&pp=1001',
+    'vendorId' => 3633435,
+    'vendor' => 'LEXAND',
+    'isGroup' => false,
+    'reviewsCount' => 61,
+    'rating' => 4.5,
+    'offersCount' => 27,
+    'gradeCount' => 66,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  7 => 
+  array (
+    'id' => 13074427,
+    'name' => 'GeoVision 5056',
+    'kind' => 'навигатор',
+    'description' => 'автомобильный, ПО: Навител, 5", MP3, фото, видео, USB, 4096Мб памяти',
+    'categoryId' => 294661,
+    'category' => 
+    array (
+      'id' => 294661,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'GPS-навигаторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '3990',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13074427.jpeg',
+      'width' => 701,
+      'height' => 461,
+    ),
+    'link' => 'https://market.yandex.ru/product/13074427?hid=294661&pp=1001',
+    'vendorId' => 431388,
+    'vendor' => 'Prestigio',
+    'isGroup' => false,
+    'reviewsCount' => 7,
+    'rating' => 4.5,
+    'offersCount' => 21,
+    'gradeCount' => 9,
+    'articlesCount' => 1,
+    'isNew' => 0,
+  ),
+  8 => 
+  array (
+    'id' => 12911340,
+    'name' => 'Montana 680',
+    'kind' => 'навигатор',
+    'description' => 'универсальный,  c видеорегистратором, ПО: Garmin, 4", GPS+ГЛОНАСС, барометр, компас, фото, USB, 4096Мб памяти',
+    'categoryId' => 294661,
+    'category' => 
+    array (
+      'id' => 294661,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'GPS-навигаторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '50290',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/12911340.jpeg',
+      'width' => 316,
+      'height' => 550,
+    ),
+    'link' => 'https://market.yandex.ru/product/12911340?hid=294661&pp=1001',
+    'vendorId' => 433112,
+    'vendor' => 'Garmin',
+    'isGroup' => false,
+    'reviewsCount' => 0,
+    'rating' => -1.0,
+    'offersCount' => 31,
+    'gradeCount' => 0,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  9 => 
+  array (
+    'id' => 8352300,
+    'name' => 'SA5',
+    'kind' => 'навигатор',
+    'description' => 'автомобильный, ПО: Навител, 5", MP3, фото, видео, USB, 4096Мб памяти',
+    'categoryId' => 294661,
+    'category' => 
+    array (
+      'id' => 294661,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'GPS-навигаторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '4290',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+      'discount' => '19',
+      'base' => '3999',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/8352300.jpeg',
+      'width' => 500,
+      'height' => 370,
+    ),
+    'link' => 'https://market.yandex.ru/product/8352300?hid=294661&pp=1001',
+    'vendorId' => 3633435,
+    'vendor' => 'LEXAND',
+    'isGroup' => false,
+    'reviewsCount' => 47,
+    'rating' => 4.5,
+    'offersCount' => 34,
+    'gradeCount' => 57,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+);
+
+
+foreach ($data as $tablet){
+	echo '<div class="data">';
+	echo '<div class="img">';
+	echo	'<img src="'.htmlspecialchars($tablet['mainPhoto']['url']).'" width="280">';	
+	echo '</div>';
+	echo "<h2><b> Информация о навигаторе: </b></h2>". "<p>". 'Модель: '  .htmlspecialchars ($tablet['name']). 'id- '. htmlspecialchars ($tablet['id']). "<p>".' рейтинг-' .
+	htmlspecialchars($tablet['rating']). "<p>". 'Описание:' .htmlspecialchars ($tablet['description']). "<p>" .' Идентификатор категории: ' .htmlspecialchars( $tablet['categoryId']). "<p>". 'цена: '.
+	number_format($tablet['prices']['amount'],2,',',' '). " ".htmlspecialchars( $tablet['prices']['curCode']).
+	"<p>". 'Поставщик: '.htmlspecialchars ($tablet['vendor']).
+	"<p> Ссылка на yandex market:" .'<a href='.htmlspecialchars($tablet['link'])."'>";
+	echo htmlspecialchars($tablet['link']);
+	echo "</a>";
+	echo '</div>' ;
+}
+
+$line=0;
+$k=0;
+$vendors=[];
+foreach ($data as $index => $tablet){
+	$line=$tablet['vendor'];
+	if(in_array($line,$vendors)){
+	}
+	else {
+		$vendors[]=$line;
+	}
+}
+
+echo "<p>" ;
+echo '<div class="data">';
+echo "<h2><b> Список производителей: </b></h2>";
+echo '</div>' ;
+foreach($vendors as $vendor){
+	$prices=[];
+	foreach ($data as $tablet){
+		if($vendor==$tablet['vendor']){
+			$prices[]=$tablet['prices']['amount'];
+		}
+	}
+	$min=100000;
+	foreach($prices as $price){
+		if($price<$min){
+			$min=$price;
+		}
+	}
+	echo '<div class="data">';
+	echo "<p>". htmlspecialchars($vendor)."</p>";
+	echo "<p>".'Минимальная цена: '  .number_format($min,2,',',' ')." ".htmlspecialchars( $tablet['prices']['curCode'])."</p>";
+	echo '</div>' ;
+}
