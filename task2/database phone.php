41dc899616ed83eac237c27f4e4ec841acb62b50
sinutkamixey <sinutkamixey>
2018-01-21 21:46:22 +0600

    var 2

---
+++ task2/database phone.php
@@ -0,0 +1,474 @@
+﻿<html>
+  <head>
+  <link rel="stylesheet" href="style.css">	
+  </head>
+  <body>
+<?php
+ $data = array (
+  0 => 
+  array (
+    'id' => 14206636,
+    'name' => 'iPhone 7 32Gb',
+    'kind' => 'смартфон',
+    'description' => '3G, iOS 10, 4.70", 1334x750, 32Гб, 138г, камера 12МП, Bluetooth',
+    'categoryId' => 91491,
+    'category' => 
+    array (
+      'id' => 91491,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мобильные телефоны',
+    ),
+    'prices' => 
+    array (
+      'amount' => '55326',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/14206636.jpeg',
+      'width' => 366,
+      'height' => 701,
+    ),
+    'link' => 'https://market.yandex.ru/product/14206636?hid=91491&pp=1001',
+    'vendorId' => 153043,
+    'vendor' => 'Apple',
+    'isGroup' => false,
+    'reviewsCount' => 45,
+    'rating' => 3.0,
+    'offersCount' => 242,
+    'gradeCount' => 162,
+    'articlesCount' => 17,
+    'isNew' => 0,
+  ),
+  1 => 
+  array (
+    'id' => 14206682,
+    'name' => 'iPhone 7 128Gb',
+    'kind' => 'смартфон',
+    'description' => '3G, iOS 10, 4.70", 1334x750, 128Гб, 138г, камера 12МП, Bluetooth',
+    'categoryId' => 91491,
+    'category' => 
+    array (
+      'id' => 91491,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мобильные телефоны',
+    ),
+    'prices' => 
+    array (
+      'amount' => '64272',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/14206682.jpeg',
+      'width' => 355,
+      'height' => 701,
+    ),
+    'link' => 'https://market.yandex.ru/product/14206682?hid=91491&pp=1001',
+    'vendorId' => 153043,
+    'vendor' => 'Apple',
+    'isGroup' => false,
+    'reviewsCount' => 34,
+    'rating' => 3.5,
+    'offersCount' => 305,
+    'gradeCount' => 71,
+    'articlesCount' => 17,
+    'isNew' => 0,
+  ),
+  2 => 
+  array (
+    'id' => 13953515,
+    'name' => 'Redmi 3S 16Gb',
+    'kind' => 'смартфон',
+    'description' => '3G, Android 6.0, 5", 1280x720, 16Гб, 144г, камера 13МП, Bluetooth',
+    'categoryId' => 91491,
+    'category' => 
+    array (
+      'id' => 91491,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мобильные телефоны',
+    ),
+    'prices' => 
+    array (
+      'amount' => '9900',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13953515.jpeg',
+      'width' => 314,
+      'height' => 588,
+    ),
+    'link' => 'https://market.yandex.ru/product/13953515?hid=91491&pp=1001',
+    'vendorId' => 7701962,
+    'vendor' => 'Xiaomi',
+    'isGroup' => false,
+    'reviewsCount' => 172,
+    'rating' => 4.5,
+    'offersCount' => 91,
+    'gradeCount' => 219,
+    'articlesCount' => 12,
+    'isNew' => 0,
+  ),
+  3 => 
+  array (
+    'id' => 14209841,
+    'name' => 'iPhone 6S 32Gb',
+    'kind' => 'смартфон',
+    'description' => '3G, iOS 9, 4.70", 1334x750, 32Гб, 143г, камера 12МП, Bluetooth',
+    'categoryId' => 91491,
+    'category' => 
+    array (
+      'id' => 91491,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мобильные телефоны',
+    ),
+    'prices' => 
+    array (
+      'amount' => '44890',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/14209841.jpeg',
+      'width' => 356,
+      'height' => 701,
+    ),
+    'link' => 'https://market.yandex.ru/product/14209841?hid=91491&pp=1001',
+    'vendorId' => 153043,
+    'vendor' => 'Apple',
+    'isGroup' => false,
+    'reviewsCount' => 4,
+    'rating' => 4.0,
+    'offersCount' => 173,
+    'gradeCount' => 13,
+    'articlesCount' => 30,
+    'isNew' => 0,
+  ),
+  4 => 
+  array (
+    'id' => 13527763,
+    'name' => 'Redmi Note 3 Pro 32Gb',
+    'kind' => 'смартфон',
+    'description' => '3G, Android 5.1, 5.50", 1920x1080, 32Гб, 164г, камера 16МП, Bluetooth',
+    'categoryId' => 91491,
+    'category' => 
+    array (
+      'id' => 91491,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мобильные телефоны',
+    ),
+    'prices' => 
+    array (
+      'amount' => '15450',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13527763.jpeg',
+      'width' => 276,
+      'height' => 516,
+    ),
+    'link' => 'https://market.yandex.ru/product/13527763?hid=91491&pp=1001',
+    'vendorId' => 7701962,
+    'vendor' => 'Xiaomi',
+    'isGroup' => false,
+    'reviewsCount' => 491,
+    'rating' => 4.5,
+    'offersCount' => 79,
+    'gradeCount' => 619,
+    'articlesCount' => 16,
+    'isNew' => 0,
+  ),
+  5 => 
+  array (
+    'id' => 13485515,
+    'name' => 'Galaxy S7 32Gb',
+    'kind' => 'смартфон',
+    'description' => '3G, Android 6.0, 5.10", 2560x1440, 32Гб, 152г, камера 12МП, Bluetooth',
+    'categoryId' => 91491,
+    'category' => 
+    array (
+      'id' => 91491,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мобильные телефоны',
+    ),
+    'prices' => 
+    array (
+      'amount' => '46190',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13485515.jpeg',
+      'width' => 284,
+      'height' => 540,
+    ),
+    'link' => 'https://market.yandex.ru/product/13485515?hid=91491&pp=1001',
+    'vendorId' => 153061,
+    'vendor' => 'Samsung',
+    'isGroup' => false,
+    'reviewsCount' => 185,
+    'rating' => 4.0,
+    'offersCount' => 165,
+    'gradeCount' => 264,
+    'articlesCount' => 28,
+    'isNew' => 0,
+  ),
+  6 => 
+  array (
+    'id' => 13340756,
+    'name' => 'Redmi Note 3 Pro 16Gb',
+    'kind' => 'смартфон',
+    'description' => '3G, Android 5.1, 5.50", 1920x1080, 16Гб, 164г, камера 16МП, Bluetooth',
+    'categoryId' => 91491,
+    'category' => 
+    array (
+      'id' => 91491,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мобильные телефоны',
+    ),
+    'prices' => 
+    array (
+      'amount' => '11700',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13340756.jpeg',
+      'width' => 276,
+      'height' => 516,
+    ),
+    'link' => 'https://market.yandex.ru/product/13340756?hid=91491&pp=1001',
+    'vendorId' => 7701962,
+    'vendor' => 'Xiaomi',
+    'isGroup' => false,
+    'reviewsCount' => 199,
+    'rating' => 4.5,
+    'offersCount' => 110,
+    'gradeCount' => 260,
+    'articlesCount' => 17,
+    'isNew' => 0,
+  ),
+  7 => 
+  array (
+    'id' => 10495456,
+    'name' => 'iPhone 5S 16Gb',
+    'kind' => 'смартфон',
+    'description' => '3G, iOS 7, 4", 1136x640, 16Гб, 112г, камера 8МП, Bluetooth',
+    'categoryId' => 91491,
+    'category' => 
+    array (
+      'id' => 91491,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мобильные телефоны',
+    ),
+    'prices' => 
+    array (
+      'amount' => '20990',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/10495456.jpeg',
+      'width' => 290,
+      'height' => 585,
+    ),
+    'link' => 'https://market.yandex.ru/product/10495456?hid=91491&pp=1001',
+    'vendorId' => 153043,
+    'vendor' => 'Apple',
+    'isGroup' => false,
+    'reviewsCount' => 444,
+    'rating' => 4.0,
+    'offersCount' => 100,
+    'gradeCount' => 3338,
+    'articlesCount' => 48,
+    'isNew' => 0,
+  ),
+  8 => 
+  array (
+    'id' => 14180410,
+    'name' => 'Redmi Note 4 64Gb',
+    'kind' => 'смартфон',
+    'description' => '3G, Android 6.0, 5.50", 1920x1080, 64Гб, 175г, камера 13МП, Bluetooth',
+    'categoryId' => 91491,
+    'category' => 
+    array (
+      'id' => 91491,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мобильные телефоны',
+    ),
+    'prices' => 
+    array (
+      'amount' => '16100',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/14180410.jpeg',
+      'width' => 324,
+      'height' => 612,
+    ),
+    'link' => 'https://market.yandex.ru/product/14180410?hid=91491&pp=1001',
+    'vendorId' => 7701962,
+    'vendor' => 'Xiaomi',
+    'isGroup' => false,
+    'reviewsCount' => 70,
+    'rating' => 4.5,
+    'offersCount' => 116,
+    'gradeCount' => 131,
+    'articlesCount' => 17,
+    'isNew' => 0,
+  ),
+  9 => 
+  array (
+    'id' => 13188751,
+    'name' => 'Galaxy A5 (2016) SM-A510F',
+    'kind' => 'смартфон',
+    'description' => '3G, Android 5.1, 5.20", 1920x1080, 16Гб, 155г, камера 13МП, Bluetooth',
+    'categoryId' => 91491,
+    'category' => 
+    array (
+      'id' => 91491,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мобильные телефоны',
+    ),
+    'prices' => 
+    array (
+      'amount' => '21989',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13188751.jpeg',
+      'width' => 340,
+      'height' => 652,
+    ),
+    'link' => 'https://market.yandex.ru/product/13188751?hid=91491&pp=1001',
+    'vendorId' => 153061,
+    'vendor' => 'Samsung',
+    'isGroup' => false,
+    'reviewsCount' => 271,
+    'rating' => 4.0,
+    'offersCount' => 171,
+    'gradeCount' => 358,
+    'articlesCount' => 11,
+    'isNew' => 0,
+  ),
+);
+
+//Вывести прайс-лист на телефоны с сортировкой по производителю — телефоны одного производителя должны идти подряд.
+echo "<H1>"."Прайс-лист на телефоны:"."</H1>";
+
+function phoneCompare($phone1, $phone2){
+		return (strcmp($phone1['vendor'], $phone2['vendor']));
+}
+usort ($data, 'phoneCompare');
+
+foreach ($data as $phone){
+	$Photo = htmlspecialchars($phone['mainPhoto']['url']);
+	$link = htmlspecialchars($phone['link']);
+	?>	
+	<table>
+		<tr> 
+			<td>
+				<img src="<?php echo $Photo ?>"/>
+			</td>
+			<td>
+				<ul type="circle">
+					<br>
+					<li><div>Марка телефона: <?= htmlspecialchars($phone['name']) ?></div></li>
+					<li><div>Описание: <?= htmlspecialchars($phone['description']) ?></div></li>
+					<li><div>Производитель: <?= htmlspecialchars($phone['vendor']) ?></div></li>
+					<li><div>Цена: <?= htmlspecialchars(number_format(($phone['prices']['amount']),2,',',' ')) ?> руб.</div></li>
+					<li><div>Рейтинг: <?= htmlspecialchars($phone['rating']) ?></div></li>
+					<li><div>Количество отзывов: <?= htmlspecialchars($phone['reviewsCount']) ?></div></li>
+					<li><a href="<?=$link;?>">Ссылка на yandex market</a> </li>
+				<ul>
+			</td>
+		</tr> 
+	</table>
+	<?php
+ }
+ 
+//Продублировать варианты с самым высоким рейтингом
+
+echo "<H2>"."Модели с самым высоким рейтингом:"."</H2>";
+$maxRating=0;
+$topPhones=[];
+Foreach ($data as $phone){
+	if (htmlspecialchars($phone['rating'])>$maxRating){
+		$maxRating=htmlspecialchars($phone['rating']);
+	}
+}
+Foreach ($data as $phone) {
+	$Photo = htmlspecialchars($phone['mainPhoto']['url']);
+	$link = htmlspecialchars($phone['link']);
+	if (htmlspecialchars($phone['rating'])== $maxRating){
+		$topPhones[]=$phone;
+		?>
+		<table>
+			<tr> 
+				<td>
+					<img src="<?php echo $Photo ?>"/>
+				</td>
+				<td>
+					<ul type="circle">
+						<br>
+						<li><div>Марка телефона: <?= htmlspecialchars($phone['name']) ?></div></li>
+						<li><div>Описание: <?= htmlspecialchars($phone['description']) ?></div></li>
+						<li><div>Производитель: <?= htmlspecialchars($phone['vendor']) ?></div></li>
+						<li><div>Цена: <?= htmlspecialchars(number_format(($phone['prices']['amount']),2,',',' ')) ?> руб.</div></li>
+						<li><div>Рейтинг: <?= htmlspecialchars($phone['rating']) ?></div></li>
+						<li><div>Количество отзывов: <?= htmlspecialchars($phone['reviewsCount']) ?></div></li>
+						<li><a href="<?=$link;?>">Ссылка на yandex market</a> </li>
+					<ul>
+				</td>
+			</tr> 
+		</table>
+		<?php
+	}
+	
+}
+
+?>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
