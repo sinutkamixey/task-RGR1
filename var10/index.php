3e333e10f90961624f68311e3d7c9b564bc9ae62
sinutkamixey <sinutkamixey>
2018-01-21 21:53:22 +0600

    var 10

---
+++ var10/index.php
@@ -0,0 +1,508 @@
+﻿<link rel="stylesheet" href="index.css">
+<?php
+ $data = array (
+  0 => 
+  array (
+    'id' => 936909,
+    'name' => 'SPS-820',
+    'kind' => 'компьютерные колонки',
+    'description' => '2.1, 38Вт, 20-20000Гц, MDF',
+    'categoryId' => 91107,
+    'category' => 
+    array (
+      'id' => 91107,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Компьютерная акустика',
+    ),
+    'prices' => 
+    array (
+      'amount' => '4180',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/936909.jpeg',
+      'width' => 698,
+      'height' => 473,
+    ),
+    'link' => 'https://market.yandex.ru/product/936909?hid=91107&pp=1001',
+    'vendorId' => 284761,
+    'vendor' => 'Sven',
+    'isGroup' => false,
+    'reviewsCount' => 329,
+    'rating' => 4.5,
+    'offersCount' => 55,
+    'gradeCount' => 1178,
+    'articlesCount' => 1,
+    'isNew' => 0,
+  ),
+  1 => 
+  array (
+    'id' => 13582969,
+    'name' => 'MS-305',
+    'kind' => 'компьютерные колонки',
+    'description' => '2.1, 40Вт, 40-20000Гц, MDF, ПДУ',
+    'categoryId' => 91107,
+    'category' => 
+    array (
+      'id' => 91107,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Компьютерная акустика',
+    ),
+    'prices' => 
+    array (
+      'amount' => '3323',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13582969.jpeg',
+      'width' => 683,
+      'height' => 456,
+    ),
+    'link' => 'https://market.yandex.ru/product/13582969?hid=91107&pp=1001',
+    'vendorId' => 284761,
+    'vendor' => 'Sven',
+    'isGroup' => false,
+    'reviewsCount' => 12,
+    'rating' => -1.0,
+    'offersCount' => 39,
+    'gradeCount' => 0,
+    'articlesCount' => 2,
+    'isNew' => 0,
+  ),
+  2 => 
+  array (
+    'id' => 10626502,
+    'name' => 'MS-1820',
+    'kind' => 'компьютерные колонки',
+    'description' => '2.1, 40Вт, ПДУ',
+    'categoryId' => 91107,
+    'category' => 
+    array (
+      'id' => 91107,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Компьютерная акустика',
+    ),
+    'prices' => 
+    array (
+      'amount' => '3512',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/10626502.jpeg',
+      'width' => 661,
+      'height' => 449,
+    ),
+    'link' => 'https://market.yandex.ru/product/10626502?hid=91107&pp=1001',
+    'vendorId' => 284761,
+    'vendor' => 'Sven',
+    'isGroup' => false,
+    'reviewsCount' => 26,
+    'rating' => 4.0,
+    'offersCount' => 43,
+    'gradeCount' => 42,
+    'articlesCount' => 3,
+    'isNew' => 0,
+  ),
+  3 => 
+  array (
+    'id' => 7774062,
+    'name' => 'MS-1085',
+    'kind' => 'компьютерные колонки',
+    'description' => '2.1, 46Вт, 40-20000Гц, MDF, ПДУ',
+    'categoryId' => 91107,
+    'category' => 
+    array (
+      'id' => 91107,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Компьютерная акустика',
+    ),
+    'prices' => 
+    array (
+      'amount' => '3970',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/7774062.jpeg',
+      'width' => 689,
+      'height' => 283,
+    ),
+    'link' => 'https://market.yandex.ru/product/7774062?hid=91107&pp=1001',
+    'vendorId' => 284761,
+    'vendor' => 'Sven',
+    'isGroup' => false,
+    'reviewsCount' => 73,
+    'rating' => 4.0,
+    'offersCount' => 48,
+    'gradeCount' => 174,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  4 => 
+  array (
+    'id' => 4743344,
+    'name' => 'SPS-607',
+    'kind' => 'компьютерные колонки',
+    'description' => 'стерео, 6Вт, 80-18000Гц, MDF',
+    'categoryId' => 91107,
+    'category' => 
+    array (
+      'id' => 91107,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Компьютерная акустика',
+    ),
+    'prices' => 
+    array (
+      'amount' => '1639',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/4743344.jpeg',
+      'width' => 613,
+      'height' => 518,
+    ),
+    'link' => 'https://market.yandex.ru/product/4743344?hid=91107&pp=1001',
+    'vendorId' => 284761,
+    'vendor' => 'Sven',
+    'isGroup' => false,
+    'reviewsCount' => 37,
+    'rating' => 4.0,
+    'offersCount' => 76,
+    'gradeCount' => 96,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  5 => 
+  array (
+    'id' => 12917945,
+    'name' => 'MS-302',
+    'kind' => 'компьютерные колонки',
+    'description' => '2.1, 40Вт, 40-20000Гц, MDF, ПДУ',
+    'categoryId' => 91107,
+    'category' => 
+    array (
+      'id' => 91107,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Компьютерная акустика',
+    ),
+    'prices' => 
+    array (
+      'amount' => '3067',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/12917945.jpeg',
+      'width' => 680,
+      'height' => 440,
+    ),
+    'link' => 'https://market.yandex.ru/product/12917945?hid=91107&pp=1001',
+    'vendorId' => 284761,
+    'vendor' => 'Sven',
+    'isGroup' => false,
+    'reviewsCount' => 5,
+    'rating' => 4.5,
+    'offersCount' => 26,
+    'gradeCount' => 7,
+    'articlesCount' => 2,
+    'isNew' => 0,
+  ),
+  6 => 
+  array (
+    'id' => 9353191,
+    'name' => 'SPS-619',
+    'kind' => 'компьютерные колонки',
+    'description' => 'стерео, 20Вт, 70-22000Гц, MDF',
+    'categoryId' => 91107,
+    'category' => 
+    array (
+      'id' => 91107,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Компьютерная акустика',
+    ),
+    'prices' => 
+    array (
+      'amount' => '2090',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/9353191.jpeg',
+      'width' => 659,
+      'height' => 541,
+    ),
+    'link' => 'https://market.yandex.ru/product/9353191?hid=91107&pp=1001',
+    'vendorId' => 284761,
+    'vendor' => 'Sven',
+    'isGroup' => false,
+    'reviewsCount' => 21,
+    'rating' => 3.5,
+    'offersCount' => 44,
+    'gradeCount' => 33,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  7 => 
+  array (
+    'id' => 7845782,
+    'name' => 'R2700',
+    'kind' => 'компьютерные колонки',
+    'description' => 'стерео, 128Вт, 20-20000Гц, MDF, ПДУ',
+    'categoryId' => 91107,
+    'category' => 
+    array (
+      'id' => 91107,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Компьютерная акустика',
+    ),
+    'prices' => 
+    array (
+      'amount' => '13414',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/7845782.jpeg',
+      'width' => 701,
+      'height' => 581,
+    ),
+    'link' => 'https://market.yandex.ru/product/7845782?hid=91107&pp=1001',
+    'vendorId' => 241845,
+    'vendor' => 'Edifier',
+    'isGroup' => false,
+    'reviewsCount' => 129,
+    'rating' => 4.0,
+    'offersCount' => 20,
+    'gradeCount' => 253,
+    'articlesCount' => 7,
+    'isNew' => 0,
+  ),
+  8 => 
+  array (
+    'id' => 6501563,
+    'name' => 'SPS-821',
+    'kind' => 'компьютерные колонки',
+    'description' => '2.1, 40Вт, 40-20000Гц, MDF',
+    'categoryId' => 91107,
+    'category' => 
+    array (
+      'id' => 91107,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Компьютерная акустика',
+    ),
+    'prices' => 
+    array (
+      'amount' => '3380',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+      'discount' => '19',
+      'base' => '2800',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/6501563.jpeg',
+      'width' => 628,
+      'height' => 331,
+    ),
+    'link' => 'https://market.yandex.ru/product/6501563?hid=91107&pp=1001',
+    'vendorId' => 284761,
+    'vendor' => 'Sven',
+    'isGroup' => false,
+    'reviewsCount' => 20,
+    'rating' => 4.5,
+    'offersCount' => 34,
+    'gradeCount' => 67,
+    'articlesCount' => 1,
+    'isNew' => 0,
+  ),
+  9 => 
+  array (
+    'id' => 3720962,
+    'name' => 'Solo 7C',
+    'kind' => 'компьютерные колонки',
+    'description' => 'стерео, 110Вт, 55-20000Гц, MDF, ПДУ',
+    'categoryId' => 91107,
+    'category' => 
+    array (
+      'id' => 91107,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Компьютерная акустика',
+    ),
+    'prices' => 
+    array (
+      'amount' => '11390',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/3720962.jpeg',
+      'width' => 323,
+      'height' => 404,
+    ),
+    'link' => 'https://market.yandex.ru/product/3720962?hid=91107&pp=1001',
+    'vendorId' => 288499,
+    'vendor' => 'Microlab',
+    'isGroup' => false,
+    'reviewsCount' => 143,
+    'rating' => 4.5,
+    'offersCount' => 18,
+    'gradeCount' => 656,
+    'articlesCount' => 4,
+    'isNew' => 0,
+  ),
+);
+?>
+<p align="center" ><b>
+<?php
+echo "ПРАЙС-ЛИСТ НА АУДИОСИСТЕМЫ";
+?>
+</b>
+</p>
+<table border align="center" cellspacing="0" cellpadding="5" cols="6" >
+	<tr>
+		<td>
+		<?php
+		echo "Название";
+		?>
+		</td>
+		<td>
+		<?php
+		echo "Описание";
+		?>
+		</td>
+		<td>
+		<?php
+		echo "Продавец";
+		?>
+		</td>
+		<td>
+		<?php
+		echo "Рейтинг";
+		?>
+		</td>
+		<td>
+		<?php
+		echo "Цена";
+		?>
+		</td>
+		<td>
+		<?php
+		echo "Фотография";
+		?>
+		</td>
+	</tr>
+	<?php
+	$newrating=[];
+	foreach($data as $item){
+		$newrating[]=$item['rating'];
+	}
+	$max=0;
+	foreach($newrating as $one){
+		if($one>$max){
+			$max=$one;
+		}
+	}
+	$min=5;
+	foreach($newrating as $one){
+		if($one>=0){
+			if($one<$min){
+			$min=$one;
+			}
+		}
+	}
+	$newprices=[];
+	foreach($data as $index => $item){
+		$newprices[$index]=$item['prices']['amount'];
+	}
+	arsort($newprices);
+
+	foreach($newprices as $index => $price){
+		foreach($data as $indexx => $item){
+			if($index==$indexx){
+			?>
+			<tr>
+				<td>
+				<span class="<?php
+				if($item['rating']==$max){
+					echo 'max-rating';
+				}
+				?>">
+				<span class="<?php
+				if($item['rating']==$min){
+					echo 'min-rating';
+				}
+				?>">
+				<?php
+				echo htmlspecialchars ($item['name']);
+				?>
+				</td>
+				<td>
+				<?php
+				echo htmlspecialchars($item['description']);
+				?>
+				</td>
+				<td>
+				<?php
+				echo htmlspecialchars ($item['vendor']);
+				?>
+				</td>
+				<td>
+				<span class="<?php
+				if($item['rating']==$max){
+					echo 'max-rating';
+				}
+				?>">
+				<span class="<?php
+				if($item['rating']==$min){
+					echo 'min-rating';
+				}
+				?>">
+				<?php
+				echo htmlspecialchars ($item['rating']);
+				?>
+				</td>
+				<td>
+				<?php
+				$formatedprice = number_format($price, 0, ',', ' ');
+				echo htmlspecialchars("$formatedprice ");
+				echo htmlspecialchars ($item['prices']['curName']);
+				?>
+				</td>
+				<td>
+				<?php
+				$photo=$item['mainPhoto']['url'];
+				$pwidth=$item['mainPhoto']['width']/6;
+				$pheight=$item['mainPhoto']['height']/6;
+				echo '<center><img src="'.htmlspecialchars($photo).'" width='.htmlspecialchars($pwidth).' height='.htmlspecialchars($pheight).'></center>';
+				?>
+				</td>
+			</tr>
+			<?php
+			}
+		}
+	}
+	?>
+</table>
