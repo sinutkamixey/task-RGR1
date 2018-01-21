a9bdf4c764b02021f291ec7bcc392791ed1e00ca
sinutkamixey <sinutkamixey>
2018-01-21 21:51:50 +0600

    var 8

---
+++ var8/index.php
@@ -0,0 +1,449 @@
+﻿<html>
+  <head>
+  <link rel="stylesheet" href="style.css">	
+  </head>
+  <body>
+ <?php
+ $data = array (
+  0 => 
+  array (
+    'id' => 12299034,
+    'name' => 'MacBook Pro 13 with Retina display Early 2015',
+    'kind' => 'ноутбук',
+    'description' => 'Core i5 / Core i7, 2600-3100 МГц, 8-16 Гб, 128-1000 Гб, 13.3 ", Intel Iris Graphics 6100, 1.58 кг, DVD нет, 4G LTE — нет, Bluetooth, Wi-Fi',
+    'categoryId' => 91013,
+    'category' => 
+    array (
+      'id' => 91013,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Ноутбуки',
+    ),
+    'prices' => 
+    array (
+      'amount' => '146427.17',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/12299034.jpeg',
+      'width' => 588,
+      'height' => 338,
+    ),
+    'link' => 'https://market.yandex.ru/product/12299034?hid=91013&pp=1001',
+    'vendorId' => 153043,
+    'vendor' => 'Apple',
+    'isGroup' => true,
+    'reviewsCount' => 29,
+    'rating' => 4.5,
+    'offersCount' => 146,
+    'gradeCount' => 59,
+    'articlesCount' => 0,
+    'isNew' => 0,
+    'modificationsCount' => 8,
+  ),
+  1 => 
+  array (
+    'id' => 13789825,
+    'name' => 'MacBook Air 13 Early 2016',
+    'kind' => 'ноутбук',
+    'description' => 'Core i5, 1600-2200 МГц, 8 Гб, 128-512 Гб, 13.3 ", Intel HD Graphics 6000, 1.35 кг, DVD нет, 4G LTE — нет, Bluetooth, Wi-Fi',
+    'categoryId' => 91013,
+    'category' => 
+    array (
+      'id' => 91013,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Ноутбуки',
+    ),
+    'prices' => 
+    array (
+      'amount' => '96117.5',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13789825.jpeg',
+      'width' => 700,
+      'height' => 414,
+    ),
+    'link' => 'https://market.yandex.ru/product/13789825?hid=91013&pp=1001',
+    'vendorId' => 153043,
+    'vendor' => 'Apple',
+    'isGroup' => true,
+    'reviewsCount' => 9,
+    'rating' => 4.5,
+    'offersCount' => 129,
+    'gradeCount' => 19,
+    'articlesCount' => 0,
+    'isNew' => 0,
+    'modificationsCount' => 3,
+  ),
+  2 => 
+  array (
+    'id' => 12753476,
+    'name' => 'IdeaPad 100 15',
+    'kind' => 'ноутбук',
+    'description' => 'Celeron / Core i3 / Core i5 / Pentium, 1900-2200 МГц, 2-6 Гб, 128-1000 Гб, 15.6 ", Intel GMA HD / Intel HD Graphics 5500 / NVIDIA GeForce 920M, 2.31 кг, DVD нет / DVD-RW, 4G LTE — нет, Bluetooth (опционально), Wi-Fi',
+    'categoryId' => 91013,
+    'category' => 
+    array (
+      'id' => 91013,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Ноутбуки',
+    ),
+    'prices' => 
+    array (
+      'amount' => '26814.93',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/12753476.jpeg',
+      'width' => 701,
+      'height' => 439,
+    ),
+    'link' => 'https://market.yandex.ru/product/12753476?hid=91013&pp=1001',
+    'vendorId' => 152981,
+    'vendor' => 'Lenovo',
+    'isGroup' => true,
+    'reviewsCount' => 44,
+    'rating' => 4.0,
+    'offersCount' => 735,
+    'gradeCount' => 73,
+    'articlesCount' => 1,
+    'isNew' => 0,
+    'modificationsCount' => 71,
+  ),
+  3 => 
+  array (
+    'id' => 12568019,
+    'name' => 'MacBook Pro 15 with Retina display Mid 2015',
+    'kind' => 'ноутбук',
+    'description' => 'Core i7, 2200-2800 МГц, 16 Гб, 256-1000 Гб, 15.4 ", AMD Radeon R9 M370X / Intel Iris Pro Graphics 5200, 2.04 кг, DVD нет, 4G LTE — нет, Bluetooth, Wi-Fi',
+    'categoryId' => 91013,
+    'category' => 
+    array (
+      'id' => 91013,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Ноутбуки',
+    ),
+    'prices' => 
+    array (
+      'amount' => '188687.59',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/12568019.jpeg',
+      'width' => 516,
+      'height' => 282,
+    ),
+    'link' => 'https://market.yandex.ru/product/12568019?hid=91013&pp=1001',
+    'vendorId' => 153043,
+    'vendor' => 'Apple',
+    'isGroup' => true,
+    'reviewsCount' => 15,
+    'rating' => 4.5,
+    'offersCount' => 98,
+    'gradeCount' => 29,
+    'articlesCount' => 0,
+    'isNew' => 0,
+    'modificationsCount' => 5,
+  ),
+  4 => 
+  array (
+    'id' => 13546772,
+    'name' => 'X540LJ',
+    'kind' => 'ноутбук',
+    'description' => 'Core i3 / Core i5, 1700-2200 МГц, 4 Гб, 500-1000 Гб, 15.6 ", NVIDIA GeForce 920M, 1.9 кг, DVD нет / DVD-RW, 4G LTE — нет, Bluetooth, Wi-Fi',
+    'categoryId' => 91013,
+    'category' => 
+    array (
+      'id' => 91013,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Ноутбуки',
+    ),
+    'prices' => 
+    array (
+      'amount' => '31833.57',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13546772.jpeg',
+      'width' => 701,
+      'height' => 368,
+    ),
+    'link' => 'https://market.yandex.ru/product/13546772?hid=91013&pp=1001',
+    'vendorId' => 152863,
+    'vendor' => 'ASUS',
+    'isGroup' => true,
+    'reviewsCount' => 1,
+    'rating' => -1.0,
+    'offersCount' => 235,
+    'gradeCount' => 0,
+    'articlesCount' => 0,
+    'isNew' => 0,
+    'modificationsCount' => 8,
+  ),
+  5 => 
+  array (
+    'id' => 13524730,
+    'name' => 'X540SA',
+    'kind' => 'ноутбук',
+    'description' => 'Celeron / Pentium, 1600 МГц, 2-8 Гб, 500-1000 Гб, 15.6 ", Intel GMA HD, 1.9 кг, DVD нет / DVD-RW, 4G LTE — нет, Bluetooth, Wi-Fi',
+    'categoryId' => 91013,
+    'category' => 
+    array (
+      'id' => 91013,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Ноутбуки',
+    ),
+    'prices' => 
+    array (
+      'amount' => '22183.36',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13524730.jpeg',
+      'width' => 701,
+      'height' => 367,
+    ),
+    'link' => 'https://market.yandex.ru/product/13524730?hid=91013&pp=1001',
+    'vendorId' => 152863,
+    'vendor' => 'ASUS',
+    'isGroup' => true,
+    'reviewsCount' => 25,
+    'rating' => 2.5,
+    'offersCount' => 470,
+    'gradeCount' => 31,
+    'articlesCount' => 0,
+    'isNew' => 0,
+    'modificationsCount' => 24,
+  ),
+  6 => 
+  array (
+    'id' => 14001094,
+    'name' => 'INSPIRON 3558',
+    'kind' => 'ноутбук',
+    'description' => 'Celeron / Core i3 / Core i5, 1600-2200 МГц, 4 Гб, 500-1000 Гб, 15.6 ", Intel GMA HD / NVIDIA GeForce 920M, 2.14 кг, DVD-RW, 4G LTE — нет, Bluetooth, Wi-Fi',
+    'categoryId' => 91013,
+    'category' => 
+    array (
+      'id' => 91013,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Ноутбуки',
+    ),
+    'prices' => 
+    array (
+      'amount' => '30564.12',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/14001094.jpeg',
+      'width' => 531,
+      'height' => 332,
+    ),
+    'link' => 'https://market.yandex.ru/product/14001094?hid=91013&pp=1001',
+    'vendorId' => 153080,
+    'vendor' => 'DELL',
+    'isGroup' => true,
+    'reviewsCount' => 3,
+    'rating' => -1.0,
+    'offersCount' => 247,
+    'gradeCount' => 0,
+    'articlesCount' => 0,
+    'isNew' => 0,
+    'modificationsCount' => 9,
+  ),
+  7 => 
+  array (
+    'id' => 13114961,
+    'name' => 'X553SA',
+    'kind' => 'ноутбук',
+    'description' => 'Celeron / Pentium, 1600 МГц, 2-4 Гб, 500-1000 Гб, 15.6 ", Intel GMA HD, 2.2 кг, DVD нет / DVD-RW, 4G LTE — нет, Bluetooth, Wi-Fi',
+    'categoryId' => 91013,
+    'category' => 
+    array (
+      'id' => 91013,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Ноутбуки',
+    ),
+    'prices' => 
+    array (
+      'amount' => '22679.5',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13114961.jpeg',
+      'width' => 435,
+      'height' => 284,
+    ),
+    'link' => 'https://market.yandex.ru/product/13114961?hid=91013&pp=1001',
+    'vendorId' => 152863,
+    'vendor' => 'ASUS',
+    'isGroup' => true,
+    'reviewsCount' => 8,
+    'rating' => 4.0,
+    'offersCount' => 250,
+    'gradeCount' => 14,
+    'articlesCount' => 0,
+    'isNew' => 0,
+    'modificationsCount' => 13,
+  ),
+  8 => 
+  array (
+    'id' => 13975941,
+    'name' => '15-ay000',
+    'kind' => 'ноутбук',
+    'description' => 'Celeron / Core i3 / Core i5 / Core i7 / Pentium, 1600-2500 МГц, 2-12 Гб, 128-1008 Гб, 15.6 ", AMD Radeon R5 M430 / AMD Radeon R7 M440 / Intel HD Graphics 400 / Intel HD Graphics 405 / Intel HD Graphics 520 / Intel HD Graphics 5500, 1.96-2.04 кг, DVD нет / DVD-RW, 4G LTE — нет, Bluetooth, Wi-Fi',
+    'categoryId' => 91013,
+    'category' => 
+    array (
+      'id' => 91013,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Ноутбуки',
+    ),
+    'prices' => 
+    array (
+      'amount' => '31500.07',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13975941.jpeg',
+      'width' => 516,
+      'height' => 348,
+    ),
+    'link' => 'https://market.yandex.ru/product/13975941?hid=91013&pp=1001',
+    'vendorId' => 152722,
+    'vendor' => 'HP',
+    'isGroup' => true,
+    'reviewsCount' => 10,
+    'rating' => 4.0,
+    'offersCount' => 792,
+    'gradeCount' => 11,
+    'articlesCount' => 0,
+    'isNew' => 0,
+    'modificationsCount' => 81,
+  ),
+  9 => 
+  array (
+    'id' => 1711753545,
+    'name' => 'MacBook Pro 13 with Retina display Late 2016',
+    'kind' => 'ноутбук',
+    'description' => 'Core i5, 2000 МГц, 8 Гб, 256 Гб, 13.3 ", Intel Iris Graphics 540, 1.37 кг, DVD нет, 4G LTE — нет, Bluetooth, Wi-Fi',
+    'categoryId' => 91013,
+    'category' => 
+    array (
+      'id' => 91013,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Ноутбуки',
+    ),
+    'prices' => 
+    array (
+      'amount' => '116995',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/1711753545.jpeg',
+      'width' => 701,
+      'height' => 658,
+    ),
+    'link' => 'https://market.yandex.ru/product/1711753545?hid=91013&pp=1001',
+    'vendorId' => 153043,
+    'vendor' => 'Apple',
+    'isGroup' => true,
+    'reviewsCount' => 14,
+    'rating' => 3.0,
+    'offersCount' => 69,
+    'gradeCount' => 26,
+    'articlesCount' => 0,
+    'isNew' => 1,
+    'modificationsCount' => 1,
+  ),
+);
+
+//1. Вывести прайс-лист на ноутбуки в порядке убывания рейтинга.
+
+echo "<H1>"."Прайс-лиcт на ноутбуки:"."</H1>";
+
+function notebooksCompare ($printer1,$printer2){
+	if ($printer2['rating']>$printer1['rating']){
+		return 1;
+	} else if ($printer2['rating']<$printer1['rating']) {
+		return  -1;
+	} else {	
+		return 0;
+	}
+}
+usort ($data, 'notebooksCompare');
+
+//найти среднюю цену ноутбуков
+
+$prices=[];
+foreach ($data as $notebookIndex => $notebook){
+		$prices[]=$notebook['prices']['amount'];
+}
+$countNotebooks=0;
+$sum=0;
+$avg=0;
+foreach($prices as $price){
+	$countNotebooks=$countNotebooks+1;
+	$sum=$sum+$price;
+}
+$avg=$sum/$countNotebooks;
+	echo 'Средняя цена ноутбуков: '. htmlspecialchars(number_format($avg,2,', ',' ')). ' руб.' . "<br><br>";
+
+
+
+foreach ($data as $notebook){
+	if ($avg>($notebook['prices']['amount'])){
+		echo '<H2> Общие характеристики: </H2>';
+		echo '<p><img src="'.htmlspecialchars($notebook['mainPhoto']['url']).'" width="100"></p>';
+		echo "<p class='large-group'>"."Название: ".htmlspecialchars( $notebook ['name'] ).
+		"<br>"."Рейтинг: ". htmlspecialchars($notebook ['rating']).
+		"<br>"."Цена: ". htmlspecialchars(number_format(($notebook ['prices']['amount']),0, ',', ' '))."  "."руб. ".
+		"<br>"."Производитель: ". htmlspecialchars($notebook ['vendor']);
+		echo "<br> <a href='".htmlspecialchars($notebook['link'])."'>Подробная информация на яндекс-маркете</a>";
+	}
+	else {
+		echo '<H2> Общие характеристики: </H2>';
+		echo '<p><img src="'.htmlspecialchars($notebook['mainPhoto']['url']).'" width="100"></p>';
+		echo "Название: ". htmlspecialchars($notebook ['name']).
+		"<br>"."Рейтинг: ". htmlspecialchars($notebook ['rating']).
+		"<br>"."Цена: ". htmlspecialchars(number_format(($notebook ['prices']['amount']),0, ',', ' '))."  "." руб. ".
+		"<br>"."Производитель: ".htmlspecialchars($notebook ['vendor']);
+		echo "<br> <a href='".htmlspecialchars($notebook['link'])."'>Подробная информация на яндекс-маркете</a>";	
+	}	
+} 
+
+
+?>
+
+
+ </body>
+</html> 
