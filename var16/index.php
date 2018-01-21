651873c96e2d7ac8e618fbf396c7b70e1ecdb58d
sinutkamixey <sinutkamixey>
2018-01-21 21:57:02 +0600

    var 16

---
+++ var16/index.php
@@ -0,0 +1,446 @@
+﻿<html>
+
+<head>
+
+<link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+<?php
+
+ $data = array (
+  0 => 
+  array (
+    'id' => 12625693,
+    'name' => 'TAB 2 A10-70L 16Gb',
+    'kind' => 'планшет',
+    'description' => '10.1", 1920x1200, Android 4.4,  16Гб, 3G, GPS, слот для карт памяти, 520г',
+    'categoryId' => 6427100,
+    'category' => 
+    array (
+      'id' => 6427100,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Планшеты',
+    ),
+    'prices' => 
+    array (
+      'amount' => '17200',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/12625693.jpeg',
+      'width' => 701,
+      'height' => 490,
+    ),
+    'link' => 'https://market.yandex.ru/product/12625693?hid=6427100&pp=1001',
+    'vendorId' => 152981,
+    'vendor' => 'Lenovo',
+    'isGroup' => false,
+    'reviewsCount' => 127,
+    'rating' => 4.0,
+    'offersCount' => 105,
+    'gradeCount' => 171,
+    'articlesCount' => 3,
+    'isNew' => 0,
+  ),
+  1 => 
+  array (
+    'id' => 13580777,
+    'name' => 'Galaxy Tab A 7.0 SM-T280 8Gb',
+    'kind' => 'планшет',
+    'description' => '7", 1280x800, Android 5.1,  8Гб, слот для карт памяти, 274г',
+    'categoryId' => 6427100,
+    'category' => 
+    array (
+      'id' => 6427100,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Планшеты',
+    ),
+    'prices' => 
+    array (
+      'amount' => '10750',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13580777.jpeg',
+      'width' => 419,
+      'height' => 701,
+    ),
+    'link' => 'https://market.yandex.ru/product/13580777?hid=6427100&pp=1001',
+    'vendorId' => 153061,
+    'vendor' => 'Samsung',
+    'isGroup' => false,
+    'reviewsCount' => 4,
+    'rating' => 4.0,
+    'offersCount' => 84,
+    'gradeCount' => 7,
+    'articlesCount' => 2,
+    'isNew' => 0,
+  ),
+  2 => 
+  array (
+    'id' => 13959730,
+    'name' => 'Galaxy Tab A 10.1 SM-T585 16Gb',
+    'kind' => 'планшет',
+    'description' => '10.1", 1920x1200, Android 6.0,  16Гб, 3G, GPS, слот для карт памяти, 525г',
+    'categoryId' => 6427100,
+    'category' => 
+    array (
+      'id' => 6427100,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Планшеты',
+    ),
+    'prices' => 
+    array (
+      'amount' => '23599',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13959730.jpeg',
+      'width' => 372,
+      'height' => 588,
+    ),
+    'link' => 'https://market.yandex.ru/product/13959730?hid=6427100&pp=1001',
+    'vendorId' => 153061,
+    'vendor' => 'Samsung',
+    'isGroup' => false,
+    'reviewsCount' => 31,
+    'rating' => 4.0,
+    'offersCount' => 98,
+    'gradeCount' => 38,
+    'articlesCount' => 1,
+    'isNew' => 0,
+  ),
+  3 => 
+  array (
+    'id' => 13584218,
+    'name' => 'iPad Pro 9.7 128Gb Wi-Fi + Cellular',
+    'kind' => 'планшет',
+    'description' => '9.7", 2048x1536, iOS,  128Гб, 3G, 444г',
+    'categoryId' => 6427100,
+    'category' => 
+    array (
+      'id' => 6427100,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Планшеты',
+    ),
+    'prices' => 
+    array (
+      'amount' => '66990',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13584218.jpeg',
+      'width' => 487,
+      'height' => 701,
+    ),
+    'link' => 'https://market.yandex.ru/product/13584218?hid=6427100&pp=1001',
+    'vendorId' => 153043,
+    'vendor' => 'Apple',
+    'isGroup' => false,
+    'reviewsCount' => 4,
+    'rating' => 4.5,
+    'offersCount' => 111,
+    'gradeCount' => 11,
+    'articlesCount' => 10,
+    'isNew' => 0,
+  ),
+  4 => 
+  array (
+    'id' => 12662455,
+    'name' => 'Galaxy Tab A 8.0 SM-T355 16Gb',
+    'kind' => 'планшет',
+    'description' => '8", 1024x768, Android 5.0,  16Гб, 3G, GPS, слот для карт памяти, 320г',
+    'categoryId' => 6427100,
+    'category' => 
+    array (
+      'id' => 6427100,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Планшеты',
+    ),
+    'prices' => 
+    array (
+      'amount' => '17269',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/12662455.jpeg',
+      'width' => 313,
+      'height' => 581,
+    ),
+    'link' => 'https://market.yandex.ru/product/12662455?hid=6427100&pp=1001',
+    'vendorId' => 153061,
+    'vendor' => 'Samsung',
+    'isGroup' => false,
+    'reviewsCount' => 45,
+    'rating' => 4.0,
+    'offersCount' => 91,
+    'gradeCount' => 61,
+    'articlesCount' => 2,
+    'isNew' => 0,
+  ),
+  5 => 
+  array (
+    'id' => 13580778,
+    'name' => 'Galaxy Tab A 7.0 SM-T285 8Gb',
+    'kind' => 'планшет',
+    'description' => '7", 1280x800, Android 5.1,  8Гб, 3G, слот для карт памяти, 274г',
+    'categoryId' => 6427100,
+    'category' => 
+    array (
+      'id' => 6427100,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Планшеты',
+    ),
+    'prices' => 
+    array (
+      'amount' => '12690',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13580778.jpeg',
+      'width' => 419,
+      'height' => 701,
+    ),
+    'link' => 'https://market.yandex.ru/product/13580778?hid=6427100&pp=1001',
+    'vendorId' => 153061,
+    'vendor' => 'Samsung',
+    'isGroup' => false,
+    'reviewsCount' => 15,
+    'rating' => 3.5,
+    'offersCount' => 128,
+    'gradeCount' => 23,
+    'articlesCount' => 1,
+    'isNew' => 0,
+  ),
+  6 => 
+  array (
+    'id' => 13175892,
+    'name' => 'TAB 2 X30L 1Gb 16Gb LTE',
+    'kind' => 'планшет',
+    'description' => '10.1", 1280x720, Android 5.1,  16Гб, 3G, GPS, слот для карт памяти, 525г',
+    'categoryId' => 6427100,
+    'category' => 
+    array (
+      'id' => 6427100,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Планшеты',
+    ),
+    'prices' => 
+    array (
+      'amount' => '12750',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13175892.jpeg',
+      'width' => 610,
+      'height' => 429,
+    ),
+    'link' => 'https://market.yandex.ru/product/13175892?hid=6427100&pp=1001',
+    'vendorId' => 152981,
+    'vendor' => 'Lenovo',
+    'isGroup' => false,
+    'reviewsCount' => 31,
+    'rating' => 4.0,
+    'offersCount' => 88,
+    'gradeCount' => 43,
+    'articlesCount' => 2,
+    'isNew' => 0,
+  ),
+  7 => 
+  array (
+    'id' => 14019515,
+    'name' => 'Tab 3 TB3-850M 2Gb 16Gb LTE',
+    'kind' => 'планшет',
+    'description' => '8", 1280x800, Android 6.0,  16Гб, 3G, GPS, слот для карт памяти, 329г',
+    'categoryId' => 6427100,
+    'category' => 
+    array (
+      'id' => 6427100,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Планшеты',
+    ),
+    'prices' => 
+    array (
+      'amount' => '12200',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/14019515.jpeg',
+      'width' => 364,
+      'height' => 601,
+    ),
+    'link' => 'https://market.yandex.ru/product/14019515?hid=6427100&pp=1001',
+    'vendorId' => 152981,
+    'vendor' => 'Lenovo',
+    'isGroup' => false,
+    'reviewsCount' => 24,
+    'rating' => 4.0,
+    'offersCount' => 77,
+    'gradeCount' => 30,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  8 => 
+  array (
+    'id' => 10546226,
+    'name' => 'iPad mini 2 32Gb Wi-Fi',
+    'kind' => 'планшет',
+    'description' => '7.9", 2048x1536, iOS,  32Гб, 331г',
+    'categoryId' => 6427100,
+    'category' => 
+    array (
+      'id' => 6427100,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Планшеты',
+    ),
+    'prices' => 
+    array (
+      'amount' => '22490',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/10546226.jpeg',
+      'width' => 479,
+      'height' => 701,
+    ),
+    'link' => 'https://market.yandex.ru/product/10546226?hid=6427100&pp=1001',
+    'vendorId' => 153043,
+    'vendor' => 'Apple',
+    'isGroup' => false,
+    'reviewsCount' => 13,
+    'rating' => 5.0,
+    'offersCount' => 44,
+    'gradeCount' => 89,
+    'articlesCount' => 17,
+    'isNew' => 0,
+  ),
+  9 => 
+  array (
+    'id' => 12670895,
+    'name' => 'Galaxy Tab E 9.6 SM-T561N 8Gb',
+    'kind' => 'планшет',
+    'description' => '9.6", 1280x800, Android 4.4,  8Гб, 3G, GPS, слот для карт памяти, 495г',
+    'categoryId' => 6427100,
+    'category' => 
+    array (
+      'id' => 6427100,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Планшеты',
+    ),
+    'prices' => 
+    array (
+      'amount' => '14500',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/12670895.jpeg',
+      'width' => 379,
+      'height' => 601,
+    ),
+    'link' => 'https://market.yandex.ru/product/12670895?hid=6427100&pp=1001',
+    'vendorId' => 153061,
+    'vendor' => 'Samsung',
+    'isGroup' => false,
+    'reviewsCount' => 37,
+    'rating' => 3.5,
+    'offersCount' => 82,
+    'gradeCount' => 55,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+);
+
+//Вывести прайс-лист на планшет в порядке возврастания цены.
+
+echo "<H1>"."Прайс-лист на планшеты:"."</H1>";
+
+function tabletsCompare($tablet1, $tablet2){
+	
+	if ($tablet1['prices']['amount'] == $tablet2['prices']['amount']){
+		return 0;
+	} else {
+		return ($tablet2['prices']['amount']<$tablet1['prices']['amount'])? 1:-1;
+	}
+}
+
+usort ($data, 'tabletsCompare');
+
+//Определить производителя, у которого больше всего предложений в списке.
+
+$max=0;
+$name=' ';
+
+Foreach ($data as $tablet){
+	
+	if (($tablet['offersCount'])>$max){
+		
+		$max=($tablet['offersCount']);
+		$name=($tablet['vendor']);
+	}
+}
+echo "<p> Максимальное число предложений: $max";
+echo "<p> От производителя: $name";
+
+//Выделить цветом предложения того производителя, у которого больше всего предложений в списке.
+//Нужно вывести основную информацию о каждой записи, включая фотографию, а также ссылку на подробную информацию на яндекс-маркете.
+
+foreach ($data as $tablet){
+	
+	if ($name==($tablet['vendor'])){
+	
+		echo '<H2> Общие характеристики планшета: </H2>';
+		echo '<p><img src="'.htmlspecialchars($tablet['mainPhoto']['url']).'" width="100"></p>';
+		echo "<p class='large-group'>"."Планшет ".htmlspecialchars( $tablet ['name'] ).
+		"<br>"."Цена ". htmlspecialchars(number_format(($tablet ['prices']['amount']),0, ',', ' '))."  "."руб. ".
+		"<br>"."Производитель ". htmlspecialchars($tablet ['vendor']).
+		"<br>"."Количество предложений ".htmlspecialchars( $tablet ['offersCount'])."</p>"; 
+		echo "<br> <a href='".htmlspecialchars($tablet['link'])."'>Подробная информация на яндекс-маркете</a>";
+	}
+	else {
+		echo '<H2> Общие характеристики планшета: </H2>';
+		echo '<p><img src="'.htmlspecialchars($tablet['mainPhoto']['url']).'" width="100"></p>';
+		echo "Планшет ". htmlspecialchars($tablet ['name']) .
+		"<br>"."Цена ". htmlspecialchars(number_format(($tablet ['prices']['amount']),0, ',', ' '))."  "." руб. ".
+		"<br>"."Производитель ".htmlspecialchars($tablet ['vendor']).
+		"<br>"."Количество предложений ". htmlspecialchars($tablet ['offersCount']); 
+		echo "<br> <a href='".htmlspecialchars($tablet['link'])."'>Подробная информация на яндекс-маркете</a>";	
+	}	
+}
+
+
+?>
+
+</body>
+</html>
