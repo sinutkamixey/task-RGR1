e48d5603ae58e996e686a3fe8c7283ae10beab0d
sinutkamixey <sinutkamixey>
2018-01-21 21:50:58 +0600

    var 7

---
+++ var7/data.php
@@ -0,0 +1,408 @@
+﻿<link rel="stylesheet" href="style.css">
+<?php
+echo "<body>";
+echo "<h1>Прайс лист на телевизоры:</h1>";
+ $data = array (
+  0 => 
+  array (
+    'id' => 12559706,
+    'name' => 'UE32J4000AU',
+    'kind' => 'телевизор',
+    'description' => '32", 1366x768, LED',
+    'categoryId' => 90639,
+    'category' => 
+    array (
+      'id' => 90639,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Телевизоры',
+    ),
+    'prices' => 
+    array (
+      'amount' => '17498',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/12559706.jpeg',
+      'width' => 701,
+      'height' => 450,
+    ),
+    'link' => 'https://market.yandex.ru/product/12559706?hid=90639&pp=1001',
+    'vendorId' => 153061,
+    'vendor' => 'Samsung',
+    'isGroup' => false,
+    'reviewsCount' => 13,
+    'rating' => 3.5,
+    'offersCount' => 70,
+    'gradeCount' => 25,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  1 => 
+  array (
+    'id' => 13909359,
+    'name' => '43UH619V',
+    'kind' => 'телевизор',
+    'description' => '43", 3840x2160, SmartTV, DVB-T2, LED, Wi-Fi, Ethernet',
+    'categoryId' => 90639,
+    'category' => 
+    array (
+      'id' => 90639,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Телевизоры',
+    ),
+    'prices' => 
+    array (
+      'amount' => '34900',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13909359.jpeg',
+      'width' => 701,
+      'height' => 457,
+    ),
+    'link' => 'https://market.yandex.ru/product/13909359?hid=90639&pp=1001',
+    'vendorId' => 153074,
+    'vendor' => 'LG',
+    'isGroup' => false,
+    'reviewsCount' => 32,
+    'rating' => 4.5,
+    'offersCount' => 48,
+    'gradeCount' => 36,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  2 => 
+  array (
+    'id' => 13884237,
+    'name' => 'UE32K5500AU',
+    'kind' => 'телевизор',
+    'description' => '32", 1920x1080, SmartTV, DVB-T2, LED, Wi-Fi, Ethernet',
+    'categoryId' => 90639,
+    'category' => 
+    array (
+      'id' => 90639,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Телевизоры',
+    ),
+    'prices' => 
+    array (
+      'amount' => '28700',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13884237.jpeg',
+      'width' => 701,
+      'height' => 458,
+    ),
+    'link' => 'https://market.yandex.ru/product/13884237?hid=90639&pp=1001',
+    'vendorId' => 153061,
+    'vendor' => 'Samsung',
+    'isGroup' => false,
+    'reviewsCount' => 21,
+    'rating' => 4.0,
+    'offersCount' => 101,
+    'gradeCount' => 27,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  3 => 
+  array (
+    'id' => 12438287,
+    'name' => 'UE40J5100AU',
+    'kind' => 'телевизор',
+    'description' => '40", 1920x1080, DVB-T2, LED',
+    'categoryId' => 90639,
+    'category' => 
+    array (
+      'id' => 90639,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Телевизоры',
+    ),
+    'prices' => 
+    array (
+      'amount' => '27010',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/12438287.jpeg',
+      'width' => 701,
+      'height' => 462,
+    ),
+    'link' => 'https://market.yandex.ru/product/12438287?hid=90639&pp=1001',
+    'vendorId' => 153061,
+    'vendor' => 'Samsung',
+    'isGroup' => false,
+    'reviewsCount' => 15,
+    'rating' => 4.5,
+    'offersCount' => 55,
+    'gradeCount' => 22,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  4 => 
+  array (
+    'id' => 13796967,
+    'name' => '49UH610V',
+    'kind' => 'телевизор',
+    'description' => '49", 3840x2160, SmartTV, DVB-T2, LED, Wi-Fi, Ethernet',
+    'categoryId' => 90639,
+    'category' => 
+    array (
+      'id' => 90639,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Телевизоры',
+    ),
+    'prices' => 
+    array (
+      'amount' => '41530',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13796967.jpeg',
+      'width' => 701,
+      'height' => 466,
+    ),
+    'link' => 'https://market.yandex.ru/product/13796967?hid=90639&pp=1001',
+    'vendorId' => 153074,
+    'vendor' => 'LG',
+    'isGroup' => false,
+    'reviewsCount' => 25,
+    'rating' => 4.0,
+    'offersCount' => 44,
+    'gradeCount' => 30,
+    'articlesCount' => 1,
+    'isNew' => 0,
+  ),
+  5 => 
+  array (
+    'id' => 10855358,
+    'name' => 'UE22H5600',
+    'kind' => 'телевизор',
+    'description' => '22", 1920x1080, SmartTV, DVB-T2, LED, Wi-Fi, Ethernet',
+    'categoryId' => 90639,
+    'category' => 
+    array (
+      'id' => 90639,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Телевизоры',
+    ),
+    'prices' => 
+    array (
+      'amount' => '15960',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/10855358.jpeg',
+      'width' => 701,
+      'height' => 450,
+    ),
+    'link' => 'https://market.yandex.ru/product/10855358?hid=90639&pp=1001',
+    'vendorId' => 153061,
+    'vendor' => 'Samsung',
+    'isGroup' => false,
+    'reviewsCount' => 44,
+    'rating' => 4.0,
+    'offersCount' => 41,
+    'gradeCount' => 67,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  6 => 
+  array (
+    'id' => 13830359,
+    'name' => '32PHT4001',
+    'kind' => 'телевизор',
+    'description' => '32", 1366x768, DVB-T2, LED, Ethernet',
+    'categoryId' => 90639,
+    'category' => 
+    array (
+      'id' => 90639,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Телевизоры',
+    ),
+    'prices' => 
+    array (
+      'amount' => '15590',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13830359.jpeg',
+      'width' => 701,
+      'height' => 467,
+    ),
+    'link' => 'https://market.yandex.ru/product/13830359?hid=90639&pp=1001',
+    'vendorId' => 152870,
+    'vendor' => 'Philips',
+    'isGroup' => false,
+    'reviewsCount' => 18,
+    'rating' => 4.0,
+    'offersCount' => 47,
+    'gradeCount' => 21,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  7 => 
+  array (
+    'id' => 13943265,
+    'name' => '32LH530V',
+    'kind' => 'телевизор',
+    'description' => '32", 1920x1080, DVB-T2, LED, Ethernet',
+    'categoryId' => 90639,
+    'category' => 
+    array (
+      'id' => 90639,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Телевизоры',
+    ),
+    'prices' => 
+    array (
+      'amount' => '18642',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13943265.jpeg',
+      'width' => 701,
+      'height' => 450,
+    ),
+    'link' => 'https://market.yandex.ru/product/13943265?hid=90639&pp=1001',
+    'vendorId' => 153074,
+    'vendor' => 'LG',
+    'isGroup' => false,
+    'reviewsCount' => 15,
+    'rating' => 4.5,
+    'offersCount' => 40,
+    'gradeCount' => 23,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  8 => 
+  array (
+    'id' => 13936163,
+    'name' => 'UE40KU6300U',
+    'kind' => 'телевизор',
+    'description' => '40", 3840x2160, SmartTV, DVB-T2, LED, Wi-Fi, Ethernet',
+    'categoryId' => 90639,
+    'category' => 
+    array (
+      'id' => 90639,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Телевизоры',
+    ),
+    'prices' => 
+    array (
+      'amount' => '41790',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13936163.jpeg',
+      'width' => 676,
+      'height' => 436,
+    ),
+    'link' => 'https://market.yandex.ru/product/13936163?hid=90639&pp=1001',
+    'vendorId' => 153061,
+    'vendor' => 'Samsung',
+    'isGroup' => false,
+    'reviewsCount' => 8,
+    'rating' => 4.5,
+    'offersCount' => 40,
+    'gradeCount' => 13,
+    'articlesCount' => 1,
+    'isNew' => 0,
+  ),
+  9 => 
+  array (
+    'id' => 10855359,
+    'name' => 'UE22H5610',
+    'kind' => 'телевизор',
+    'description' => '22", 1920x1080, SmartTV, DVB-T2, LED, Wi-Fi, Ethernet',
+    'categoryId' => 90639,
+    'category' => 
+    array (
+      'id' => 90639,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Телевизоры',
+    ),
+    'prices' => 
+    array (
+      'amount' => '16718',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/10855359.jpeg',
+      'width' => 701,
+      'height' => 458,
+    ),
+    'link' => 'https://market.yandex.ru/product/10855359?hid=90639&pp=1001',
+    'vendorId' => 153061,
+    'vendor' => 'Samsung',
+    'isGroup' => false,
+    'reviewsCount' => 51,
+    'rating' => 4.0,
+    'offersCount' => 41,
+    'gradeCount' => 88,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+);
+
+$diagonals=[];
+foreach($data as $index => $tv){
+	$diagonals[$index] = substr($tv ['description'], 0, 2);
+}
+arsort($diagonals);
+
+foreach($diagonals as $index => $diagonal){
+	foreach ($data as $tvindex => $tv){
+		if($index==$tvindex){
+			echo '<div class="data">';
+			echo '<div class="img">';
+			echo '<img src="'.htmlspecialchars($tv['mainPhoto']['url']).'" width="280">';	
+			echo '</div>';
+			echo "<h2><b> Основные характеристики: </b></h2>". 
+			"<p>". 'Марка телевизора: '  .htmlspecialchars ($tv['name']). 
+			"<p>".'Идентификатор модели: '.htmlspecialchars ($tv['id']). 
+			"<p>".'Рейтинг: ' .htmlspecialchars($tv['rating']).
+			"<p>". 'Описание: ' .htmlspecialchars ($tv['description']).
+			"<p>" .' Идентификатор категории: ' .htmlspecialchars( $tv['categoryId']). 
+			"<p>". 'Цена: '.number_format($tv['prices']['amount'],2,',',' '). " ".htmlspecialchars( $tv['prices']['curCode']).
+			"<p>". 'Поставщик: '.htmlspecialchars ($tv['vendor']).
+			"<p>".' Код поставщика: '.htmlspecialchars( $tv['vendorId'] ).
+			"<p>" .'Количество: '.htmlspecialchars( $tv['gradeCount']).
+			"<p> Ссылка на yandex market:" .'<a href='.htmlspecialchars($tv['link'])."'>";
+			echo htmlspecialchars($tv['link']);
+			echo "</a>";
+			echo '</div>' ;
+		}
+	}
+}
+echo "</body>";
