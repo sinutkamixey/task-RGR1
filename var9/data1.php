48501065cc9697ce8580c59529aa6f4df37e529d
sinutkamixey <sinutkamixey>
2018-01-21 21:52:35 +0600

    var 9

---
+++ var9/data1.php
@@ -0,0 +1,416 @@
+﻿<link rel="stylesheet" href="style.css">
+<?php
+ $data = array (
+  0 => 
+  array (
+    'id' => 13579255,
+    'name' => '25UM58',
+    'kind' => 'монитор',
+    'description' => 'TFT IPS, 25", 2560x1080, HDMI',
+    'categoryId' => 91052,
+    'category' => 
+    array (
+      'id' => 91052,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мониторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '12533',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13579255.jpeg',
+      'width' => 701,
+      'height' => 403,
+    ),
+    'link' => 'https://market.yandex.ru/product/13579255?hid=91052&pp=1001',
+    'vendorId' => 153074,
+    'vendor' => 'LG',
+    'isGroup' => false,
+    'reviewsCount' => 11,
+    'rating' => 4.0,
+    'offersCount' => 49,
+    'gradeCount' => 16,
+    'articlesCount' => 1,
+    'isNew' => 0,
+  ),
+  1 => 
+  array (
+    'id' => 7349236,
+    'name' => 'U2412M',
+    'kind' => 'монитор',
+    'description' => 'TFT E-IPS, 24", 1920x1200, DVI, DisplayPort, VGA',
+    'categoryId' => 91052,
+    'category' => 
+    array (
+      'id' => 91052,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мониторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '18990',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/7349236.jpeg',
+      'width' => 568,
+      'height' => 594,
+    ),
+    'link' => 'https://market.yandex.ru/product/7349236?hid=91052&pp=1001',
+    'vendorId' => 153080,
+    'vendor' => 'DELL',
+    'isGroup' => false,
+    'reviewsCount' => 151,
+    'rating' => 4.5,
+    'offersCount' => 121,
+    'gradeCount' => 341,
+    'articlesCount' => 4,
+    'isNew' => 0,
+  ),
+  2 => 
+  array (
+    'id' => 13576910,
+    'name' => '29UM68',
+    'kind' => 'монитор',
+    'description' => 'TFT IPS, 29", 2560x1080, HDMI, DisplayPort',
+    'categoryId' => 91052,
+    'category' => 
+    array (
+      'id' => 91052,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мониторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '20860',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13576910.jpeg',
+      'width' => 701,
+      'height' => 434,
+    ),
+    'link' => 'https://market.yandex.ru/product/13576910?hid=91052&pp=1001',
+    'vendorId' => 153074,
+    'vendor' => 'LG',
+    'isGroup' => false,
+    'reviewsCount' => 5,
+    'rating' => 4.5,
+    'offersCount' => 46,
+    'gradeCount' => 5,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  3 => 
+  array (
+    'id' => 13793418,
+    'name' => 'C24F390FHI',
+    'kind' => 'монитор',
+    'description' => 'TFT *VA, 23.5", 1920x1080, HDMI, VGA',
+    'categoryId' => 91052,
+    'category' => 
+    array (
+      'id' => 91052,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мониторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '13196',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13793418.jpeg',
+      'width' => 701,
+      'height' => 528,
+    ),
+    'link' => 'https://market.yandex.ru/product/13793418?hid=91052&pp=1001',
+    'vendorId' => 153061,
+    'vendor' => 'Samsung',
+    'isGroup' => false,
+    'reviewsCount' => 13,
+    'rating' => 5.0,
+    'offersCount' => 34,
+    'gradeCount' => 13,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  4 => 
+  array (
+    'id' => 10461526,
+    'name' => '223V5LSB',
+    'kind' => 'монитор',
+    'description' => 'TFT TN, 21.5", 1920x1080, DVI, VGA',
+    'categoryId' => 91052,
+    'category' => 
+    array (
+      'id' => 91052,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мониторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '6599',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/10461526.jpeg',
+      'width' => 701,
+      'height' => 529,
+    ),
+    'link' => 'https://market.yandex.ru/product/10461526?hid=91052&pp=1001',
+    'vendorId' => 152870,
+    'vendor' => 'Philips',
+    'isGroup' => false,
+    'reviewsCount' => 11,
+    'rating' => 3.5,
+    'offersCount' => 123,
+    'gradeCount' => 21,
+    'articlesCount' => 2,
+    'isNew' => 0,
+  ),
+  5 => 
+  array (
+    'id' => 10634270,
+    'name' => 'U2414H',
+    'kind' => 'монитор',
+    'description' => 'TFT IPS, 23.8", 1920x1080, HDMI, DisplayPort, miniDisplayPort',
+    'categoryId' => 91052,
+    'category' => 
+    array (
+      'id' => 91052,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мониторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '17890',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/10634270.jpeg',
+      'width' => 701,
+      'height' => 659,
+    ),
+    'link' => 'https://market.yandex.ru/product/10634270?hid=91052&pp=1001',
+    'vendorId' => 153080,
+    'vendor' => 'DELL',
+    'isGroup' => false,
+    'reviewsCount' => 49,
+    'rating' => 4.5,
+    'offersCount' => 62,
+    'gradeCount' => 82,
+    'articlesCount' => 3,
+    'isNew' => 0,
+  ),
+  6 => 
+  array (
+    'id' => 11924637,
+    'name' => 'U2515H',
+    'kind' => 'монитор',
+    'description' => 'TFT IPS, 25", 2560x1440, HDMI, DisplayPort, miniDisplayPort',
+    'categoryId' => 91052,
+    'category' => 
+    array (
+      'id' => 91052,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мониторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '25427',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/11924637.jpeg',
+      'width' => 701,
+      'height' => 580,
+    ),
+    'link' => 'https://market.yandex.ru/product/11924637?hid=91052&pp=1001',
+    'vendorId' => 153080,
+    'vendor' => 'DELL',
+    'isGroup' => false,
+    'reviewsCount' => 27,
+    'rating' => 4.5,
+    'offersCount' => 49,
+    'gradeCount' => 28,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  7 => 
+  array (
+    'id' => 10433427,
+    'name' => 'VX239H',
+    'kind' => 'монитор',
+    'description' => 'TFT AH-IPS, 23", 1920x1080, HDMI, VGA',
+    'categoryId' => 91052,
+    'category' => 
+    array (
+      'id' => 91052,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мониторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '13610',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/10433427.jpeg',
+      'width' => 472,
+      'height' => 353,
+    ),
+    'link' => 'https://market.yandex.ru/product/10433427?hid=91052&pp=1001',
+    'vendorId' => 152863,
+    'vendor' => 'ASUS',
+    'isGroup' => false,
+    'reviewsCount' => 36,
+    'rating' => 4.5,
+    'offersCount' => 56,
+    'gradeCount' => 80,
+    'articlesCount' => 1,
+    'isNew' => 0,
+  ),
+  8 => 
+  array (
+    'id' => 10378101,
+    'name' => 'GW2760HS',
+    'kind' => 'монитор',
+    'description' => 'TFT A-MVA, 27", 1920x1080, DVI, HDMI, VGA',
+    'categoryId' => 91052,
+    'category' => 
+    array (
+      'id' => 91052,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мониторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '13150',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/10378101.jpeg',
+      'width' => 701,
+      'height' => 554,
+    ),
+    'link' => 'https://market.yandex.ru/product/10378101?hid=91052&pp=1001',
+    'vendorId' => 241228,
+    'vendor' => 'BenQ',
+    'isGroup' => false,
+    'reviewsCount' => 28,
+    'rating' => 5.0,
+    'offersCount' => 71,
+    'gradeCount' => 74,
+    'articlesCount' => 2,
+    'isNew' => 0,
+  ),
+  9 => 
+  array (
+    'id' => 13542119,
+    'name' => '24MP58VQ',
+    'kind' => 'монитор',
+    'description' => 'TFT IPS, 23.8", 1920x1080, DVI, HDMI, VGA',
+    'categoryId' => 91052,
+    'category' => 
+    array (
+      'id' => 91052,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Мониторы',
+    ),
+    'prices' => 
+    array (
+      'amount' => '11500',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13542119.jpeg',
+      'width' => 564,
+      'height' => 444,
+    ),
+    'link' => 'https://market.yandex.ru/product/13542119?hid=91052&pp=1001',
+    'vendorId' => 153074,
+    'vendor' => 'LG',
+    'isGroup' => false,
+    'reviewsCount' => 3,
+    'rating' => -1.0,
+    'offersCount' => 90,
+    'gradeCount' => 0,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+);
+echo '<div class="text1">';
+echo "<h1><b> Прайс - лист: </b></h1>". "<p>";
+echo '</div>';
+
+function TabletsCompare ($amount1,$amount2){
+	if ($amount2['prices']['amount']<$amount1['prices']['amount']){
+		return 1;
+	} else if ($amount2['prices']['amount']>$amount1['prices']['amount']) {
+		return  -1;
+	} else {	
+		return 0;
+	}
+}
+usort ($data, 'TabletsCompare');
+foreach ($data as $monitor){
+	echo '<div class="text">';
+	if ($monitor['rating'] >= 4.5) {
+		echo '<div class="img">';
+		echo '<img src="'. htmlspecialchars($monitor['mainPhoto']['url']).'" width="280">';
+		echo '</div>';
+	}
+		echo "<h2><b> Общие характеристики: </b></h2>". "<p>". 
+		'Монитор: '  .htmlspecialchars($monitor['name']). 'id '. htmlspecialchars ($monitor['id']). "<p>".
+		' Рейтинг: ' .htmlspecialchars($monitor['rating']). "<p>". 
+		'Характеристики: ' .htmlspecialchars($monitor['description']). "<p>" .
+		' Код категории: ' .htmlspecialchars( $monitor['categoryId']). "<p>". 
+		'Стоимость: '.number_format($monitor['prices']['amount'],2,',',' '). " ".htmlspecialchars( $monitor['prices']['curName']).
+		"<p>". 'Производитель: '.htmlspecialchars($monitor['vendor']).
+		"<p>" .'Количество: '.htmlspecialchars( $monitor['gradeCount']).
+		"<p> Ссылка на Yandex Market:" .'<a href='.htmlspecialchars($monitor['link'])."'>" ;
+		echo htmlspecialchars($monitor['link']);
+		echo "</a>";
+		echo '</div>';
+	}
+	
+
+
+
+	  
+	
+
+
